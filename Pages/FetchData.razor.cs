543638b5d6f3b2c962c635e10688abc97fed5091
Pablo <pelinski46@gmail.com>
2024-08-29 10:26:05 -0300

    initial commit

---
+++ Pages/FetchData.razor.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Net.Http.Json;
+
+namespace Portfolio.Pages;
+
+public partial class FetchData
+{
+    List<HistoryItem>? repositories;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        repositories = await Http.GetFromJsonAsync<List<HistoryItem>>("https://api.github.com/users/pelinski46/repos");
+    }
+
+    class HistoryItem
+    {
+        public string Full_Name { get; set; }
+        public string Description { get; set; }
+        public string HTML_URL { get; set; }
+
+
+    }
+}
